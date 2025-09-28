67f321c27d049f7d8a9dfd24cbc8524ab1b60937
SiriBelavigi <92957131+SiriBelavigi@users.noreply.github.com>
2025-09-27 20:23:04 -0700

    added the model

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import numpy as np
 import pickle 
 IMAGE_ADDRESS = "https://prdmspst.blob.core.windows.net/images/articles/swimming-walking-effectively-reduce-pain-for-fibromyalgia-e0c8628b-f81c-4688-baeb-1071f6e11065-thumbnail.jpg"
-#def load_sklearn_model(model_path):
-    #with open(model_path, "rb") as model_file:
-        #final_model = pickle.load(model_file)
-    #return final_model
+def load_sklearn_model(model_path):
+    with open(model_path, "rb") as model_file:
+        final_model = pickle.load(model_file)
+    return final_model
 
-#classification_model = load_sklearn_model("")
+classification_model = load_sklearn_model("Fibromyalgia_app_model")
 
 def run_app():
     st.title("Fibromyalgia Predicitons")
@@ -22,12 +22,12 @@ def run_app():
     SPSa_total = st.number_input("enter SPSa score", min_value = 0, value = 50)
     SPSb_total = st.number_input("enter SPSb score", min_value = 0, value = 50)
     features = np.array([[gender_feature, age, CSI_total, SAT_total, SPS_total, SPSa_total, SPSb_total]])
-    #if st.button("predict"):
-        #model_predict = classification_model.predict(features)
-        #result = model_predict[0]
-        #if result == 1:
-            #class_name = "fibromyalgia" 
-        #else: class_name = "undercontrol"
-        #st.success(f"predicition: {class_name}")
+    if st.button("predict"):
+        model_predict = classification_model.predict(features)
+        result = model_predict[0]
+        if result == 1:
+            class_name = "fibromyalgia" 
+        else: class_name = "undercontrol"
+        st.success(f"predicition: {class_name}")
 
 run_app()
