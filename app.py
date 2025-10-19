18a5d754d5b93b932d719695d7a665e1283fb75f
SiriBelavigi <92957131+SiriBelavigi@users.noreply.github.com>
2025-10-18 19:55:59 -0700

    added the scaled versions of the features in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pickle 
+from sklearn.preprocessing import StandardScaler
 IMAGE_ADDRESS = "https://prdmspst.blob.core.windows.net/images/articles/swimming-walking-effectively-reduce-pain-for-fibromyalgia-e0c8628b-f81c-4688-baeb-1071f6e11065-thumbnail.jpg"
 def load_sklearn_model(model_path):
     with open(model_path, "rb") as model_file:
@@ -8,6 +9,12 @@ def load_sklearn_model(model_path):
     return final_model
 
 classification_model = load_sklearn_model("Fibromyalgia_app_model")
+
+def load_scaler(scalerpath):
+    with open(scalerpath, "rb") as scaler_file:
+       scaler = pickle.load(scaler_file)
+    return scaler
+scaler = load_scaler("standard_scaler")
 
 def run_app():
     st.title("Fibromyalgia Predicitons")
@@ -22,8 +29,10 @@ def run_app():
     SPSa_total = st.number_input("enter SPSa score", min_value = 0, value = 50)
     SPSb_total = st.number_input("enter SPSb score", min_value = 0, value = 50)
     features = np.array([[gender_feature, age, CSI_total, SAT_total, SPS_total, SPSa_total, SPSb_total]])
+    scaled_features = scaler.transform(features)
+    final_features = np.hstack([[[gender_feature]],scaled_features])
     if st.button("predict"):
-        model_predict = classification_model.predict(features)
+        model_predict = classification_model.predict(final_features)
         result = model_predict[0]
         if result == 1:
             class_name = "fibromyalgia" 
